6019df2e9fcc144351129831d949a7a29c65a85d
Oleksandr Kovalenko <oleksandr.kovalenko@growbots.com>
2018-03-09 12:03:15 +0100

    Clean hamiltonian evaluation logic

--- numerov/ceo.py
+++ numerov/ceo.py
@@ -1,16 +1,20 @@
 import numpy as np
+from time import time
 import scipy.linalg as spla
 import matplotlib.pyplot as plt
-from time import time
 
 # NB: That's how one can separate physical model from 
 # parameters of the algorithm
 class CeoPotential:
-    def __init__(self, omega=1, d=0, mag=30.0, width=2.0):
+    def __init__(self, omega=1, d=0, mag=30.0, width=2.0, hbar=1, mass=1):
         self.omega = omega
         self.width = width
         self.mag = mag
         self.d = d
+
+        # NB: All units should be members of Potentai classes
+        self.hbar = hbar
+        self.mass = mass
 
     def _ceo_dopping(self, x, x0):
         ceo_pot = self.mag / self.width * x - self.mag * (1 + x0 / self.width)
@@ -20,7 +24,7 @@ class CeoPotential:
     def _harmonic(self, x, x0):
         return 0.5 * self.omega**2 * (x - x0)**2 + self.d
 
-    def evaluate(self, x, x0):
+    def potential(self, x, x0):
         return self._harmonic(x, x0) + self._ceo_dopping(x, -10) 
 
 
@@ -29,68 +33,56 @@ def diagonalize_hamiltonian(Hamiltonian):
 
 # TODO: Move find_E to separate class
 #
-def find_E(potential, xvec, x0, steps, h, hbar, m, Binv):
-    # create the potential 
-    U = potential.evaluate(xvec, x0)
+class Solver:
+    def __init__(self, task, size, stepsize):
+        self.task = task 
+        A = self.ndiag(-2.0, size) / stepsize ** 2
+        B = self.ndiag(10.0, size) / 12.0
+        # Free hamiltonian term in numerov method
+        # $ -\frac{\hbar^{2}}{2m}B^{-1}A \psi $
+        self.free_hamiltonian = -0.5 * task.hbar ** 2 / task.mass * spla.inv(B).dot(A) 
 
-    # create Laplacian via 3-point finite-difference method
-    Laplacian=(-2.0 
-        * np.diag(np.ones(steps))
-        + np.diag(np.ones(steps - 1), 1)
-        + np.diag(np.ones(steps - 1), -1)
-    ) / float(h**2)
+    @staticmethod
+    def ndiag(coef, size):
+        # Create diagonal matrix for 3-point finite-difference method
+        return (coef
+            * np.diag(np.ones(size))
+            + np.diag(np.ones(size - 1), 1)
+            + np.diag(np.ones(size - 1), -1)
+        )
 
-    # create the Hamiltonian
-    Hamiltonian=np.zeros((steps,steps))
-    [im,jm]=np.indices(Hamiltonian.shape)
-    Hamiltonian[im==jm]=U
-    Hamiltonian += -0.5 * (hbar**2) / m * Binv.dot(Laplacian)
-    # diagonalize the Hamiltonian yielding the wavefunctions and energies
-    E, V = diagonalize_hamiltonian(Hamiltonian)
-    # determine theoretical number of energy levels (n)
-    return (E,V,U)
+    def evaluate(self, xvec, x0):
+        # evaluate the potential 
+        U = self.task.potential(xvec, x0)
 
-def calculate_energy_psi(nx0):
-    steps = 1000
-    hbar = 1.0
-    m = 1.0
+        # evaluate the Hamiltonian
+        # $$ -\frac{\hbar^{2}}{2m}B^{-1}A \psi + U \psi = E \psi$$
+        hamiltonian = self.free_hamiltonian + np.diag(U)
+
+        # diagonalize the Hamiltonian yielding the wavefunctions and energies
+        E, V = diagonalize_hamiltonian(hamiltonian)
+        return E, V, U
+
+def calculate_energy_psi(nx0, nsteps=1000):
     W = 20
     A = W/2.0
-    xvec = np.linspace(-A, A, steps)
+    xvec = np.linspace(-A, A, nsteps)
     # get step size
-    h = xvec[1]-xvec[0]
-
-    # B matrix - to convert Numerov equation into eigenvalue problem
-    Bmatrix = (10.0 
-        * np.diag(np.ones(steps))
-        + np.diag(np.ones(steps-1), 1)
-        + np.diag(np.ones(steps-1), -1)
-    ) / float(12.0)
-
-    Binv = spla.inv(Bmatrix)
-
-    #x0 = 0
-    #E,V,U = find_E(xvec,x0,steps,h,hbar,m,Binv)
-
-    # print output
-    #output(E,n)
-    # create plot
-    #finite_well_plot(E,V,xvec,steps,n,U)
+    stepsize = xvec[1] - xvec[0]
 
     t0 = time()
-    x0vec=np.linspace(-20,5,nx0)
-    Eall = np.zeros((steps,nx0))
+    x0vec = np.linspace(-20, 5, nx0)
+    Eall = np.zeros((nsteps, nx0))
 
-    psiall = np.zeros((len(xvec),steps,len(x0vec)))
-    potential = CeoPotential()
-    for i in range(len(x0vec)):
-        x0 = x0vec[i]
-        Evec,Vvec,Uvec = find_E(potential, xvec, x0, steps, h, hbar,m,Binv)
+    psiall = np.zeros((len(xvec),nsteps,len(x0vec)))
+    solver = Solver(CeoPotential(), nsteps, stepsize)
+    for i, x0 in enumerate(x0vec):
+        Evec,Vvec,Uvec = solver.evaluate(xvec, x0)
         psiall[:,:,i] = Vvec[:,:]**2
         Eall[:,i] = Evec[:]
     print('Calculation time: {0:.3f}'.format(time() - t0), 's')
-
     return Eall, psiall, xvec, x0vec
+
 
 #def density(Eall, psiall, xvec, x0vec):
 
